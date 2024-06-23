da1a8fa95654304706e3fab4ff0bb1d3aef0154a
ebrarkiziloglu <ebrarkiziloglu@gmail.com>
2024-06-23 14:46:24 +0300

    add navigation icons and move main function to __init__.py

--- SOR/browser.py
+++ SOR/browser.py
@@ -1,7 +1,9 @@
 from PyQt5.QtWebEngineWidgets import QWebEngineView
 from PyQt5.QtCore import QUrl
 
+
 class Browser(QWebEngineView):
+
     def __init__(self):
         super().__init__()
         self.setUrl(QUrl("https://www.google.com"))
